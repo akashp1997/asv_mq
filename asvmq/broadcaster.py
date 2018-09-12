883f97f77198b52bff148c0f0db12f08201358f0
Akash Purandare <akash.p1997@gmail.com>
2018-09-12 16:37:31 +0530

    Corrected the name of the class and cleaned and fixed the codes

--- asvmq/broadcaster.py
+++ asvmq/broadcaster.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import pika
 
-class Broadcaster(object):
-    def __init__(self, hostname="localhost", port=5672, topic_name="", type_object=str):
+class Publisher(object):
+    def __init__(self, topic_name="", type_object=str, hostname="localhost", port=5672):
         self._parameters = pika.ConnectionParameters(hostname, port)
         self._connection = None
         self._channel = None
         self._name_exchange = topic_name
         self._object_type = type_object
+        if (self._name_exchange):
+            self._exchange_type = "fanout"
+        else:
+            self._exchange_type = "direct"
 
     def close(self):
         """Destroys the channel only"""
@@ -39,6 +43,11 @@ class Broadcaster(object):
         return self._name_exchange
 
     @property
+    def exchange_type(self):
+        """This method returns the exchange type"""
+        return self._exchange_type
+
+    @property
     def type(self):
         """Returns the type of object to be strictly followed by the Publisher to send"""
         return self._object_type
@@ -48,13 +57,17 @@ class Broadcaster(object):
         if (self._channel==None):
             self._connection = pika.BlockingConnection(self._parameters)
             self._channel = self._connection.channel()
-        self._channel.exchange_declare(exchange=self._name_exchange,exchange_type="fanout")
+        self._channel.exchange_declare(exchange=self._name_exchange,exchange_type=self._exchange_type)
 
     def publish(self, message):
         """Method for publishing the message to the MQ Broker"""
         if(type(message)!=self.type):
             raise ValueError("Please ensure that the message passed to this method is of the same type as defined during the exchange declaration")
-        string = message.SerializeToString().decode()
-        success = self._channel.basic_publish(exchange=self.topic,routing_key="", body=string)
+        if(type(message)!=str):
+            try:
+                message = message.SerializeToString().decode()
+            except:
+                raise ValueError("Are you sure that the message is Protocol Buffer message/string?")
+        success = self._channel.basic_publish(exchange=self.topic,routing_key="", body=message)
         if(not success):
             print("Cannot deliver message to Exchange")
