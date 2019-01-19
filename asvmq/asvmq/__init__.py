d121f87eeb19c4ffac787665c998f2e3d572573d
Akash Purandare <akash.p1997@gmail.com>
2019-01-19 09:38:15 +0530

    Deleted redundant asvmq folder

--- asvmq/asvmq/__init__.py
+++
@@ -1,7 +0,0 @@
-'''This python package contains the Python Module version number
-and it contains the major base classes required to do communication in the ASV'''
-__version__ = "0.1.4"
-
-import topic_communications as topic
-
-import work_queue_communications as wqueue
