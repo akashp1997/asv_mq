171b3bcfee6886dd3749a1d99e7dd233ec6eb091
Akash Purandare <akash.p1997@gmail.com>
2019-01-31 20:11:36 +0530

    Reflected version number

--- asvmq/__init__.py
+++ asvmq/__init__.py
@@ -1,5 +1,5 @@
 '''This python package contains the Python Module version number
 and it contains the major base classes required to do communication in the ASV'''
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
-from .topic_communications import Publisher, Subscriber, logDebug, logInfo, logWarn, logFatal
+from .topic_communications import Publisher, Subscriber, log_debug, log_info, log_warn, log_fatal
